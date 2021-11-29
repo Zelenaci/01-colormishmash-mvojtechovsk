981362c0197940bb6ddb65a3d430d97f7add2a04
Michal <voj38077@spseol.cz>
2021-11-29 15:49:32 +0100

    ColorMishMash úkol

--- main.py
+++ main.py
@@ -18,32 +18,42 @@ class Application(tk.Tk):
 
         self.bind("<Escape>", self.quit)
 
-        self.lblR = tk.Label(self, text="R")
+        self.lblR = tk.Label(self, text="R", font='Helvetica 18 bold')
         self.lblR.pack()
-        self.scaleR = Scale(from_=0, to=255,orient = HORIZONTAL, length = 200, command = self.change)
+        self.scaleR = Scale(from_=0, to=255,orient = HORIZONTAL, length = 200, command = self.change,background='#000000')
         self.scaleR.pack()
 
-        self.lblG = tk.Label(self, text="G")
+        self.lblG = tk.Label(self, text="G", font='Helvetica 18 bold')
         self.lblG.pack()
-        self.scaleG = Scale(from_=0, to=255,orient = HORIZONTAL, length = 200)
+        self.scaleG = Scale(from_=0, to=255,orient = HORIZONTAL, length = 200, command = self.change,background='#000000')
         self.scaleG.pack()
 
-        self.lblB = tk.Label(self, text="B")
+        self.lblB = tk.Label(self, text="B", font='Helvetica 18 bold')
         self.lblB.pack()
-        self.scaleB = Scale(from_=0, to=255,orient = HORIZONTAL, length = 100)
+        self.scaleB = Scale(from_=0, to=255,orient = HORIZONTAL, length = 200, command = self.change,background='#000000')
         self.scaleB.pack()
 
-        self.canvas = Canvas(self,width = 256, height = 100, background = '#000000') 
-        self.canvas.pack()  
 
         self.btn = tk.Button(self, text="Quit", command=self.quit)
         self.btn.pack()
 
+        self.geometry('350x350')
+        self.configure(background = '#000000')
+
     def change(self,event):
         r = self.scaleR.get()
-        g = self.scaleR.get()
-        b = self.scaleR.get()
-        self.canvas.config(background =f'#{r:02x}{g:02x}{b:02x}')
+        g = self.scaleG.get()
+        b = self.scaleB.get()
+
+        self.configure(background =f'#{r:02x}{g:02x}{b:02x}')
+        
+        self.scaleR.configure(background =f'#{r:02x}{g:02x}{b:02x}')
+        self.scaleG.configure(background =f'#{r:02x}{g:02x}{b:02x}')
+        self.scaleB.configure(background =f'#{r:02x}{g:02x}{b:02x}')
+
+        self.lblR.configure(background =f'#{r:02x}{g:02x}{b:02x}')
+        self.lblG.configure(background =f'#{r:02x}{g:02x}{b:02x}')
+        self.lblB.configure(background =f'#{r:02x}{g:02x}{b:02x}')
 
     def quit(self, event=None):
         super().quit()
