6563595919396896f4fc5835c71be8c7cf7f12aa
Michal <voj38077@spseol.cz>
2021-12-13 09:33:04 +0100

    rozmístění + fce

--- main.py
+++ main.py
@@ -2,10 +2,8 @@
 
 from os.path import basename, splitext
 import tkinter as tk
-from tkinter import Scale, HORIZONTAL, Canvas
-
+from tkinter import Scale, HORIZONTAL, Canvas, Frame, Entry,LEFT,S,END,StringVar
 # from tkinter import ttk
-
 
 
 class Application(tk.Tk):
@@ -16,44 +14,71 @@ class Application(tk.Tk):
         super().__init__(className=self.name)
         self.title(self.name)
 
+        self.frameR = Frame(self)
+        self.frameR.pack()
+        self.frameG = Frame(self)
+        self.frameG.pack()
+        self.frameB = Frame(self)
+        self.frameB.pack()
+
         self.bind("<Escape>", self.quit)
 
-        self.lblR = tk.Label(self, text="R", font='Helvetica 18 bold')
-        self.lblR.pack()
-        self.scaleR = Scale(from_=0, to=255,orient = HORIZONTAL, length = 200, command = self.change,background='#000000')
-        self.scaleR.pack()
+        self.lblR = tk.Label(self.frameR, text="R", font='Helvetica 18 bold')
+        self.lblR.pack(side = LEFT,anchor=S)
+        self.scaleR = Scale(self.frameR,from_=0, to=255, orient=HORIZONTAL,
+                            length = 200, command=self.change,)
+        self.varR = StringVar()
+        self.scaleR.pack(side=LEFT, anchor = S)
+        self.entryR = Entry(self.frameR,width = 6,textvariable = self.varR)
+        self.entryR.pack(side = LEFT,anchor=S)
+   
 
-        self.lblG = tk.Label(self, text="G", font='Helvetica 18 bold')
-        self.lblG.pack()
-        self.scaleG = Scale(from_=0, to=255,orient = HORIZONTAL, length = 200, command = self.change,background='#000000')
-        self.scaleG.pack()
+        self.lblG = tk.Label(self.frameG, text="G", font='Helvetica 18 bold')
+        self.lblG.pack(side = LEFT,anchor=S)
+        self.scaleG = Scale(self.frameG,from_=0, to=255, orient=HORIZONTAL,
+                            length = 200,command=self.change,)
+        self.varG = StringVar()                            
+        self.scaleG.pack(side=LEFT)
+        self.entryG = Entry(self.frameG,width = 6,textvariable = self.varG)
+        self.entryG.pack(side = LEFT,anchor=S)
 
-        self.lblB = tk.Label(self, text="B", font='Helvetica 18 bold')
-        self.lblB.pack()
-        self.scaleB = Scale(from_=0, to=255,orient = HORIZONTAL, length = 200, command = self.change,background='#000000')
-        self.scaleB.pack()
 
+        self.lblB = tk.Label(self.frameB, text="B", font='Helvetica 18 bold')
+        self.lblB.pack(side = LEFT,anchor=S)
+        self.scaleB = Scale(self.frameB,from_=0, to=255, orient=HORIZONTAL,
+                            length = 200, command=self.change,)
+        self.varB = StringVar()                    
+        self.scaleB.pack(side=LEFT)
+        self.entryB = Entry(self.frameB,width = 6,textvariable = self.varB)
+        self.entryB.pack(side=LEFT,anchor=S)
+
+        self.entryMain = Entry(self)
+        self.entryMain.pack(side=LEFT)
 
         self.btn = tk.Button(self, text="Quit", command=self.quit)
         self.btn.pack()
 
         self.geometry('350x350')
-        self.configure(background = '#000000')
+        self.configure(background='#000000')
 
-    def change(self,event):
+    def change(self, event):
         r = self.scaleR.get()
         g = self.scaleG.get()
         b = self.scaleB.get()
 
-        self.configure(background =f'#{r:02x}{g:02x}{b:02x}')
-        
-        self.scaleR.configure(background =f'#{r:02x}{g:02x}{b:02x}')
-        self.scaleG.configure(background =f'#{r:02x}{g:02x}{b:02x}')
-        self.scaleB.configure(background =f'#{r:02x}{g:02x}{b:02x}')
+        colorcode = f'#{r:02x}{g:02x}{b:02x}'
 
-        self.lblR.configure(background =f'#{r:02x}{g:02x}{b:02x}')
-        self.lblG.configure(background =f'#{r:02x}{g:02x}{b:02x}')
-        self.lblB.configure(background =f'#{r:02x}{g:02x}{b:02x}')
+        self.entryMain.delete(0,END)
+        self.entryMain.insert(0, colorcode)
+
+        self.varR.set(r)
+        self.varG.set(g)
+        self.varB.set(b)
+
+
+        self.configure(background=f'#{r:02x}{g:02x}{b:02x}')
+
+        
 
     def quit(self, event=None):
         super().quit()
